ac1e521890b82484f9f8d9cf2dd869d9323c1f11
akrao <akrao@zynga.com>
2015-09-27 01:45:59 +0530

    Project 0 - My App Portfolio

---
+++ app/src/main/java/com/example/akshatha/myappportfolio/app/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.akshatha.myappportfolio.app;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the button */
+    public void showToast(View view) {
+        // Do something in response to button click
+        Context ctx = getApplicationContext();
+        CharSequence txt = "Coming Soon!!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(ctx, txt, duration);
+        toast.show();
+        toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
+    }
+}
